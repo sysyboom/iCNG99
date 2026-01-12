6001b6a2b05a4cb0ee4abccd1270a4b80b116156
sysyboom <114929833+sysyboom@users.noreply.github.com>
2026-01-12 16:52:44 +0800

    Update combined_unique.m

--- code/combined_unique.m
+++ code/combined_unique.m
@@ -1,24 +1,9 @@
-
-% 获取文件夹中所有的.mat文件
 files = dir(fullfile('ggtrans*.mat'));
-
-% 初始化一个空数组来存储所有数据
 allData = [];
-
-% 循环遍历每个文件
 for i = 1:length(files)
-    % 构建完整的文件路径
     filePath = fullfile(files(i).name);
-    
-  % 加载.mat文件中的数据
-    data = load(filePath, 'resultss');  % 指定加载名为 resultss 的变量
-    
-    % 将加载的数据追加到allData数组中
-    allData = [allData; data.resultss(:)];  % 确保数据是一维的
+    data = load(filePath, 'resultss'); 
+    allData = [allData; data.resultss(:)]; 
 end
-
-% 去除重复的数据
 uniqueData = unique(allData);
-
-% 保存合并后的数据到一个新的.mat文件中
 save(fullfile( 'combinedData.mat'), 'uniqueData'); 
