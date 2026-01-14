969aa3175131f89e72cfa1c73f34ad8ea7afcd0e
sysyboom <114929833+sysyboom@users.noreply.github.com>
2026-01-14 20:28:24 +0800

    Update and rename deleteone.py to drug_target_test.py

--- code/analysis/drug_target_test.py
+++ code/analysis/drug_target_test.py
@@ -1,34 +1,24 @@
 import cobra
 import pytest
 
-
-# 定义 model fixture
-@pytest.fixture
 def model():
-    # 加载模型
     model = cobra.io.read_sbml_model('/mnt/NFS/fengch/new/models/merge_after.xml')
     return model
 
-
-# 测试敲除基因是否会影响目标函数
 def test_gene_knockout(model):
     gene_id_to_knockout = 'CNAG_06508'
 
-    # 检查基因是否存在
     try:
         gene = model.genes.get_by_id(gene_id_to_knockout)
     except KeyError:
         print(f"Gene {gene_id_to_knockout} not found in the model.")
         return False
 
-    # 敲除基因并优化模型
     model.genes.get_by_id(gene_id_to_knockout).knock_out()
     solution = model.optimize()
 
-    # 输出目标函数流值
     print(f"Objective value after knocking out {gene_id_to_knockout}: {solution.objective_value}")
 
-    # 判断是否有流
     if solution.objective_value > 1e-6:
         print(f"After knocking out {gene_id_to_knockout}, the network still has flux through the objective function.")
         return True
